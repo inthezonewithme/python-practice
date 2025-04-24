1513ac4dbc6fc555cc491a0a4c7597d50203ee32
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-04-24 13:47:47 +0300

    Changed the input of the sum function example call to a list formerly a tuple Example function calls of factorial

--- factorial.py
+++ factorial.py
@@ -1,9 +1,13 @@
 def factorial(n):
     fact=1
+
+    #multiply each decremental number from n up to 1 then returns the factorial
     while n>0:
         fact = fact*n
         n = n-1
     return fact
 
-
+#example of using the function
+print(factorial(5))
+print(factorial(6))
 
